2e1f4ba42473c691795e399d7fb278e49bf98f1d
Jokers77 <pet.edward77@gmail.com>
2015-12-07 15:40:25 +0100

    add comment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,8 @@
 class Post < ActiveRecord::Base
   belongs_to :user
-
-  has_attached_file :image, style: { thumb: '120x120', large: '300x400' }
+  has_many :comments
+  has_attached_file :image, style: { medium: '700x500', small: '350x250' }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+
 
 end
