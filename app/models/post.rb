268c1ef8370248f3960e37384ffac548fd167e55
Jokers77 <pet.edward77@gmail.com>
2015-12-07 14:07:41 +0100

    cfg paperclip and view acces

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
   belongs_to :user
 
-  has_attached_file :image
+  has_attached_file :image, style: { thumb: '120x120', large: '300x400' }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
 end
